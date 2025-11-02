3e35bf2c51d4d021baa2206e7456f1aa21ce470f
Joey Yu <joey@jyu.dev>
2025-11-02 14:56:47 -0500

    30s timeout

--- internal/discord/discord.go
+++ internal/discord/discord.go
@@ -225,7 +225,7 @@ func (b *DiscordBot) handleAsk(
 		return
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	options := i.ApplicationCommandData().Options

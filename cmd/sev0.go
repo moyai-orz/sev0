e1252eb8a67969a5622e11b31893d7e4f05595aa
Joey Yu <joey@jyu.dev>
2025-11-01 21:43:12 -0400

    init

--- cmd/sev0.go
+++ cmd/sev0.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"sev0/internal/discord"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Failed loading *.env file")
+	}
+
+	bot, err := discord.NewDiscordBot()
+	if err != nil {
+		slog.Error("Failed to create discord bot", "err", err)
+		return
+	}
+
+	if err := bot.Start(); err != nil {
+		slog.Error("Failed to start discord bot", "err", err)
+		return
+	}
+	defer bot.Close()
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
---
+++ internal/discord/discord.go
@@ -0,0 +1,67 @@
+// Package discord hosts the SEV0 bot
+package discord
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type DiscordBot struct {
+	Session *discordgo.Session
+}
+
+func NewDiscordBot() (*DiscordBot, error) {
+	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	if err != nil {
+		slog.Error("Failed initializing discordgo", "err", err)
+		return nil, err
+	}
+
+	bot := &DiscordBot{Session: dg}
+	bot.Session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentMessageContent
+
+	bot.Session.AddHandler(bot.messageIngest)
+
+	return bot, nil
+}
+
+func (b *DiscordBot) Start() error {
+	err := b.Session.Open()
+	if err != nil {
+		return fmt.Errorf("error opening connection: %w", err)
+	}
+	return nil
+}
+
+func (b *DiscordBot) Close() {
+	if err := b.Session.Close(); err != nil {
+		slog.Error("error closing discord session", "err", err)
+	}
+}
+
+func (b *DiscordBot) messageIngest(
+	s *discordgo.Session,
+	m *discordgo.MessageCreate,
+) {
+	if m.GuildID == "" {
+		// Ignore DMs
+		return
+	}
+
+	if m.Author.ID == s.State.User.ID {
+		// Ignore itself
+		return
+	}
+
+	jsonData, err := json.MarshalIndent(m, "", "  ")
+	if err != nil {
+		slog.Error("failed to marshal message to json", "err", err)
+		return
+	}
+
+	slog.Info(string(jsonData))
+}

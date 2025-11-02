30c7e9e7830c4b6de1290a0c216d43aab0079793
Joey Yu <joey@jyu.dev>
2025-11-01 22:16:19 -0400

    ingest

--- cmd/sev0.go
+++ cmd/sev0.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"sev0/ent"
 	"sev0/internal/discord"
 
 	"github.com/joho/godotenv"
@@ -15,22 +17,35 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		slog.Error("Failed loading *.env file")
+		slog.Error("failed loading *.env file")
 	}
 
-	bot, err := discord.NewDiscordBot()
+	entClient, err := ent.Open(
+		"postgres",
+		os.Getenv("DATABASE_URL"),
+	)
 	if err != nil {
-		slog.Error("Failed to create discord bot", "err", err)
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer entClient.Close()
+	// Run the auto migration tool.
+	if err := entClient.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	bot, err := discord.NewDiscordBot(entClient)
+	if err != nil {
+		slog.Error("failed to create discord bot", "err", err)
 		return
 	}
 
 	if err := bot.Start(); err != nil {
-		slog.Error("Failed to start discord bot", "err", err)
+		slog.Error("failed to start discord bot", "err", err)
 		return
 	}
 	defer bot.Close()
 
-	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	slog.Info("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
--- internal/discord/discord.go
+++ internal/discord/discord.go
@@ -2,26 +2,31 @@
 package discord
 
 import (
-	"encoding/json"
-	"fmt"
+	"context"
 	"log/slog"
 	"os"
+	"time"
+
+	"sev0/ent"
+	"sev0/ent/discordmessage"
+	"sev0/ent/discorduser"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 type DiscordBot struct {
-	Session *discordgo.Session
+	Session   *discordgo.Session
+	entClient *ent.Client
 }
 
-func NewDiscordBot() (*DiscordBot, error) {
+func NewDiscordBot(entClient *ent.Client) (*DiscordBot, error) {
 	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		slog.Error("Failed initializing discordgo", "err", err)
 		return nil, err
 	}
 
-	bot := &DiscordBot{Session: dg}
+	bot := &DiscordBot{Session: dg, entClient: entClient}
 	bot.Session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentMessageContent
 
 	bot.Session.AddHandler(bot.messageIngest)
@@ -32,7 +37,9 @@ func NewDiscordBot() (*DiscordBot, error) {
 func (b *DiscordBot) Start() error {
 	err := b.Session.Open()
 	if err != nil {
-		return fmt.Errorf("error opening connection: %w", err)
+		slog.Error("error opening connection", "err", err)
+		return err
+
 	}
 	return nil
 }
@@ -61,11 +68,28 @@ func (b *DiscordBot) messageIngest(
 		return
 	}
 
-	jsonData, err := json.MarshalIndent(m, "", "  ")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	userID, err := b.entClient.DiscordUser.Create().
+		SetGlobalName(m.Author.GlobalName).
+		SetUsername(m.Author.Username).
+		SetID(m.Author.ID).
+		OnConflictColumns(discorduser.FieldID).
+		UpdateNewValues().
+		ID(ctx)
 	if err != nil {
-		slog.Error("failed to marshal message to json", "err", err)
-		return
+		slog.Error("failed to create discord user: ", "err", err)
 	}
 
-	slog.Info(string(jsonData))
+	err = b.entClient.DiscordMessage.Create().
+		SetID(m.ID).
+		SetContent(m.Content).
+		SetUserID(userID).
+		SetTimestamp(m.Timestamp).
+		SetEditedTimestamp(*m.EditedTimestamp).
+		OnConflictColumns(discordmessage.FieldID).UpdateNewValues().Exec(ctx)
+	if err != nil {
+		slog.Error("failed to create discord message: ", "err", err)
+	}
 }
